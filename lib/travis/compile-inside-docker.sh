dddbeee3c857593adac3e4e4e828927d2aa78120
Edward Lee <e45lee@uwaterloo.ca>
2016-12-21 13:32:23 -0500

    [build] Fix typo.

--- lib/travis/compile-inside-docker.sh
+++ lib/travis/compile-inside-docker.sh
@@ -12,7 +12,7 @@ cmake -DCMAKE_TOOLCHAIN_FILE=/usr/emsdk_portable/emscripten/tag-1.36.1/cmake/Mod
 	-DLLVM_ENABLE_THREADS=0 -DLLVM_TARGETS_TO_BUILD=${ARCH} -DLLVM_TARGET_ARCH=${ARCH} \
 	-DEMSCRIPTEN_PRELOAD=/usr/emsdk_portable/emscripten/tag-1.36.1/tools/file_packager.py \
 	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
-	-DPATH_TO_LLVM_TBLGEN=../patch/llvm-${LLVM_VERSION}-tblgen -DCLANG_TBLGEN=../patch-clang-${CLANG_VERSION}-tblgen \
+	-DPATH_TO_LLVM_TBLGEN=../patch/llvm-${LLVM_VERSION}-tblgen -DCLANG_TBLGEN=../patch-clang-${LLVM_VERSION}-tblgen \
 	/usr/src
 
 make -j2 install
