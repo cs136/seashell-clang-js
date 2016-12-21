877ee8128914ed3bd39d5fe0c62caa0ecb82b528
Edward Lee <e45lee@uwaterloo.ca>
2016-12-21 13:55:55 -0500

    [build] Fix paths.

--- lib/travis/compile-inside-docker.sh
+++ lib/travis/compile-inside-docker.sh
@@ -7,10 +7,10 @@ mkdir build
 cd build
 
 echo "Starting build in docker container..."
-cmake -DCMAKE_TOOLCHAIN_FILE=/usr/emsdk_portable/emscripten/tag-${EMSCRIPTEN_VERSION}/cmake/Modules/Platform/Emscripten.cmake \
+cmake -DCMAKE_TOOLCHAIN_FILE=/emsdk_portable/emscripten/tag-${EMSCRIPTEN_VERSION}/cmake/Modules/Platform/Emscripten.cmake \
 	-DCMAKE_CROSSCOMPILING=True -DCMAKE_INSTALL_PREFIX=install \
 	-DLLVM_ENABLE_THREADS=0 -DLLVM_TARGETS_TO_BUILD=${ARCH} -DLLVM_TARGET_ARCH=${ARCH} \
-	-DEMSCRIPTEN_PRELOAD=/usr/emsdk_portable/emscripten/tag-${EMSCRIPTEN_VERSION}/tools/file_packager.py \
+	-DEMSCRIPTEN_PRELOAD=/emsdk_portable/emscripten/tag-${EMSCRIPTEN_VERSION}/tools/file_packager.py \
 	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
 	-DPATH_TO_LLVM_TBLGEN=../patch/llvm-${LLVM_VERSION}-tblgen -DCLANG_TBLGEN=../patch-clang-${LLVM_VERSION}-tblgen \
 	/usr/src
