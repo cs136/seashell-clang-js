0e633950bf23f374c7c9fd7242446cde17cbcc14
Edward Lee <e45lee@uwaterloo.ca>
2016-12-21 14:22:23 -0500

    [build] Fix typo.

--- lib/travis/compile-inside-docker.sh
+++ lib/travis/compile-inside-docker.sh
@@ -7,7 +7,7 @@ mkdir build
 cd build
 
 echo "Starting build in docker container..."
-/emsdk_portable/emscripten/tag-${EMSCRIPTEN_VERSION}/emcmake \
+/emsdk_portable/emscripten/tag-${EMSCRIPTEN_VERSION}/emcmake cmake \
 	-DCMAKE_CROSSCOMPILING=True -DCMAKE_INSTALL_PREFIX=install \
 	-DLLVM_ENABLE_THREADS=0 -DLLVM_TARGETS_TO_BUILD=${ARCH} -DLLVM_TARGET_ARCH=${ARCH} \
 	-DEMSCRIPTEN_PRELOAD=/emsdk_portable/emscripten/tag-${EMSCRIPTEN_VERSION}/tools/file_packager.py \
