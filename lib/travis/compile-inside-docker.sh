c20432aad991977e3e323905a08c72aee374626c
Edward Lee <e45lee@uwaterloo.ca>
2016-12-21 14:34:41 -0500

    [build] Fix build arguments.

--- lib/travis/compile-inside-docker.sh
+++ lib/travis/compile-inside-docker.sh
@@ -12,7 +12,7 @@ echo "Starting build in docker container..."
 	-DLLVM_ENABLE_THREADS=0 -DLLVM_TARGETS_TO_BUILD=${ARCH} -DLLVM_TARGET_ARCH=${ARCH} \
 	-DEMSCRIPTEN_PRELOAD=/emsdk_portable/emscripten/tag-${EMSCRIPTEN_VERSION}/tools/file_packager.py \
 	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
-	-DPATH_TO_LLVM_TBLGEN=../patch/llvm-${LLVM_VERSION}-tblgen -DCLANG_TBLGEN=../patch-clang-${LLVM_VERSION}-tblgen \
+	-DLLVM_TABLGEN=../patch/llvm-${LLVM_VERSION}-tblgen -DCLANG_TABLEGEN=../patch-clang-${LLVM_VERSION}-tblgen \
 	/usr/src
 
 make -j2 install
