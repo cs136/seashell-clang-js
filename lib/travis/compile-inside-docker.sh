06ea7f16b41938beb7c16d76849a09ee7bdb7ef0
Edward Lee <e45lee@uwaterloo.ca>
2016-12-22 15:06:26 -0500

    [build] Adjust outlining limit, build with MinSizeRel.

--- lib/travis/compile-inside-docker.sh
+++ lib/travis/compile-inside-docker.sh
@@ -2,9 +2,6 @@
 LLVM_VERSION=3.9
 ARCH=X86
 
-# Set umask so that debugging is easier
-umask 000
-
 cd /usr/src
 mkdir build
 cd build
@@ -24,7 +21,7 @@ ln -s `which ccache` /usr/lib/ccache/em++
 
 echo "Starting build in docker container..."
 /emsdk_portable/emscripten/tag-${EMSCRIPTEN_VERSION}/emcmake cmake \
-  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_BUILD_TYPE=MinSizeRel \
   -DCMAKE_C_COMPILER=/usr/lib/ccache/emcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/em++ \
 	-DCMAKE_CROSSCOMPILING=True -DCMAKE_INSTALL_PREFIX=install \
 	-DLLVM_ENABLE_THREADS=0 -DLLVM_TARGETS_TO_BUILD=${ARCH} -DLLVM_TARGET_ARCH=${ARCH} \
